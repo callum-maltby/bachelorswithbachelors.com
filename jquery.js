541d7e044daa78e02fd9b2c2f38679d76ce50ab3
Callum Maltby <cmaltby@student.unimelb.edu.au>
2017-03-17 00:03:00 +1100

    Initial commit
    Features present:
    - Development log is hardcoded on grey cards
    - Basic JQuery is running to fade in/out the monlogue in index.html

---
+++ jquery.js
@@ -0,0 +1,43 @@
+$(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+		//quotes.eq(quoteIndex % quotes.length)
+		quotes.eq(quoteIndex)
+            .fadeIn(2000)
+            .delay(2000)
+			.fadeOut(2000, showNextQuote);
+			
+			//if (quoteIndex === quotes.length) .stop(true)
+			
+    }
+
+    showNextQuote();
+
+})();
+
+$(document).ready(function(){
+	$('#page_effect').fadeIn(5000);
+	$('#page_effect').fadeOut(5000);
+});
+
+(function() {
+    var cnt = 0;
+    var specials = $(".special_button");
+    
+    function next() {
+        if (cnt < 5) {
+            specials.fadeTo(2000, .1).fadeTo(2000, 1, next);
+            ++cnt;
+        }                    
+    }
+    
+    next();
+})();
+
+$("#monologue_fade").fadeOut(function() {
+  $(this).text("World").fadeIn();
+});
